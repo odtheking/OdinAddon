9f3a2db0490f34ece160e894875ba0e2abed55ca
odtheking <odtheking1@gmail.com>
2025-12-27 18:56:15 +0200

    Fixed syntax

--- src/main/kotlin/com/odtheking/odinaddon/OdinAnimations.kt
+++ src/main/kotlin/com/odtheking/odinaddon/OdinAnimations.kt
@@ -9,17 +9,8 @@ import net.fabricmc.api.ClientModInitializer
 object OdinAnimations : ClientModInitializer {
 
     override fun onInitializeClient() {
-        println("Odin Addon initialized!")
-
-        // Register commands by adding to the array
-        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
-            arrayOf(odinAddonCommand).forEach { commodore -> commodore.register(dispatcher) }
-        }
-
-        // Register objects to event bus by adding to the list
         listOf(this).forEach { EventBus.subscribe(it) }
 
-        // Register modules by adding to the list
         ModuleManager.registerModules(ModuleConfig("OdinAddon.json"), Animations)
     }
 }
