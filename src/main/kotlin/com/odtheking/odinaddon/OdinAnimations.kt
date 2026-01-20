d24e8aa8d9336d0edcd875ac6a0b2336d8f2edab
Hayoumi <40734629+Hayoumi@users.noreply.github.com>
2026-01-21 06:01:32 +0700

    Add files via upload

--- src/main/kotlin/com/odtheking/odinaddon/OdinAnimations.kt
+++ src/main/kotlin/com/odtheking/odinaddon/OdinAnimations.kt
@@ -5,12 +5,23 @@ import com.odtheking.odin.events.core.EventBus
 import com.odtheking.odin.features.ModuleManager
 import com.odtheking.odinaddon.features.impl.render.Animations
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 
 object OdinAnimations : ClientModInitializer {
+    private val config = ModuleConfig("OdinAddon.json")
 
     override fun onInitializeClient() {
         listOf(this).forEach { EventBus.subscribe(it) }
 
-        ModuleManager.registerModules(ModuleConfig("OdinAddon.json"), Animations)
+        ModuleManager.registerModules(config, Animations)
+
+        ClientPlayConnectionEvents.DISCONNECT.register { _, _ ->
+            config.save()
+        }
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register {
+            config.save()
+        }
     }
 }
